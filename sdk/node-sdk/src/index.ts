315990f1e8d53f11bbf0e77a9dc1eaf3c14e3b1d
Yoanm <4410697+yoanm@users.noreply.github.com>
2024-08-23 10:05:31 +0200

    Improve

--- sdk/node-sdk/src/index.ts
+++ sdk/node-sdk/src/index.ts
@@ -5,8 +5,8 @@ export * as path from './path';
 export * as load from './load';
 export * as array from './array';
 export * as merge from './merge';
-export * as format from './format';
-export * as filter from './filter';
+//export * as format from '.format';
+//export * as filter from '.filter';
 
 export * from './constants';
 export * from './interfaces';
